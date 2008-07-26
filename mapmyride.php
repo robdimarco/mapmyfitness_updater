47e079d67ac5b9829c83ea9badef469c66806df1
Rob Di Marco <rob@innovationontherun.com>
2008-07-26 15:20:46 -0400

    Added classes

---
+++ mapmyride.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('MAGPIE_DIR')) {
+    define('MAGPIE_DIR', dirname(__FILE__) . DIRECTORY_SEPARATOR . "magpierss");
+}
+if (!defined('MAP_MY_RIDE_RSS_URL')) {
+    define('MAP_MY_RIDE_RSS_URL','http://www.mapmyride.com/rss/rss_workouts?u=');
+}
+
+require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "workout.php");
+
+class MapMyRide {
+    var $userId;
+    var $workouts;
+    function MapMyRide($userId = ''){
+        $this->$userId = $userId;
+        $this->workouts = array();
+    }
+    function fetch_data() {
+        if (!function_exists('fetch_rss')) {
+            require_once (MAGPIE_DIR . DIRECTORY_SEPARATOR ."rss_fetch.inc");
+        }
+        $rss = fetch_rss(MAP_MY_RIDE_RSS_URL . $this->userId);
+	    if ( is_array( $rss->items ) && !empty( $rss->items ) ) {
+		    foreach ($rss->items as $item ) {
+		        $this->workouts[] = new Workout($item);
+		    }
+	    } else {
+	        throw new Exception('Could not find any RSS items for user Id :'. $this->userId);
+	    }
+    }
+}
+?>
---
+++ workout.php
@@ -0,0 +1,37 @@
+<?php
+class Workout {
+    var $workoutId;
+    var $title;
+    var $link;
+    var $detailArray;
+    function Workout($rssItem = '') {
+        $this->detailArray = array();
+        if (!'' == $rssItem) {
+            $this->setDetailsFromRssItem($rssItem);
+        }
+    }
+    function setDetailsFromRssItem($rssItem){
+        $this->title = strip_tags($rssItem['title']);
+        $this->link  = $rssItem['link'];
+        $this->setDetailArrayFromString($rssItem['description']);
+    }
+
+    function setDetailArrayFromString($str) {
+        $workoutDetails = preg_split("/<br *\/>/",str_replace(array("\n", "\r"), ' ', $str));
+        foreach ($workoutDetails as $workout) {
+			$vals = preg_split("/:?<\/?b *>/", $workout);
+			if ($vals[1]) {
+			    $this->detailArray[strip_tags(html_entity_decode($vals[1], ENT_QUOTES))] = strip_tags(html_entity_decode($vals[2], ENT_QUOTES));
+            }
+        }
+    }
+    function parseLinkForId($link = ''){
+        if ($link == '') {
+            $link = $this ->link;
+        }
+        if (preg_match('/txtWorkoutID\=[0-9]+/',$link, $matches)) {
+            $this->workoutId = substr($matches[0],strlen("txtWorkoutID="));
+        }
+    }
+}
+?>
