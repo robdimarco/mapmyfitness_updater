b167f292077fe762d469bbadfbcbd77d58f40ece
Rob Di Marco <rob@innovationontherun.com>
2008-08-13 10:52:40 -0400

    Updated to have alters to use friendfeed

--- sql/mmr.sql
+++ sql/mmr.sql
@@ -5,3 +5,11 @@ create table mmr_ff_user(id int auto_increment primary key,
     last_workout_id int, 
     last_update_date timestamp,
     is_active boolean default true);
+    
+    
+rename table mmr_ff_user to mmr_notify_user;
+alter table mmr_notify_user change friend_feed_user_name app_user_name varchar(255) not null;
+alter table mmr_notify_user change friend_feed_auth_key  app_auth_key  varchar(255) not null;
+alter table mmr_notify_user add notify_type varchar(50);
+update mmr_notify_user set notify_type ='friendfeed';
+alter table mmr_notify_user modify notify_type varchar(50) not null;
