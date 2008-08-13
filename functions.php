fa849cb90a07b2b8d28ae80ab24d4e2edc7310fa
Rob Di Marco <rob@innovationontherun.com>
2008-08-13 11:24:38 -0400

    Added shorten url function

--- functions.php
+++ functions.php
@@ -11,4 +11,25 @@ function addWorkoutLinkToFriendFeed($ff, $mmr, $id, $intro ="Just finished a wor
       }
 
 }
+function addWorkoutLinkToTwitter($twitter, $mmr, $id, $intro ="Just finished a workout: ") {
+      $workout = null;
+      foreach ($mmr->workouts as $wo) {
+          if ($wo->workoutId == $id) {
+              $workout = $wo;
+          }
+      }
+      if (null != $workout) {
+          $twitter->status($intro . $workout->title . " " . getShortenedUrl($workout->link));
+      }
+}
+
+function getShortenedUrl($urlToConvert) {
+	$url="http://bit.ly/api?url=" . $urlToConvert;
+    $fileopen=file($url);
+    $count=count($fileopen);
+	for($i=0;$i<$count;$i++){
+		$filegets.=$fileopen[$i];
+	}
+	return $filegets;
+}
 ?>
